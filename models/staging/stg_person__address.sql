2567648c4b262c7d285007269cd587c7612fa663
Doug <doug.sgrott@gmail.com>
2025-01-16 18:03:12 -0300

    Adding first staging model for the project.

---
+++ models/staging/stg_person__address.sql
@@ -0,0 +1,12 @@
+with
+    stg_address as (
+        select
+            addressid as address_id
+            , stateprovinceid as state_province_id
+            , coalesce(addressline1, '') || ', ' || coalesce(addressline1, '') AS addressline_full
+            , city
+            , postalcode
+        from {{ source('person', 'address') }}
+    )
+select *
+from stg_address
